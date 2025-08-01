976ddee609d2d27af0f85d9dff312517157c1da2
hellstar-bit <juanluiscamposimanca@gmail.com>
2025-08-01 11:00:26 -0500

    Update Dashboard.tsx

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import {
   CurrencyDollarIcon,
   UsersIcon,
-  ArrowArrowTrendingUpIcon,
+  ArrowTrendingUpIcon,
   ExclamationTriangleIcon,
   DocumentArrowUpIcon,
   BanknotesIcon,
